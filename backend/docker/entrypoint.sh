6bbf50da8a6058eb9a65add246b460484db78ff8
tebanmendz1 <tebanmendz1@users.noreply.github.com>
2026-08-05 10:10:57 -0400

    Run database migrations during API startup

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 mkdir -p storage/framework/cache/data storage/framework/sessions storage/framework/views storage/logs
 
+php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
